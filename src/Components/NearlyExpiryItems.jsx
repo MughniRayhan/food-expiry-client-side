b7bf25007288f3c94f6ef4e2a99d8ca268b50a7a
MughniRayhan <tishtisha13@gmail.com>
2025-06-13 18:08:02 +0600

    Implement NearlyExpiryItems component to display items nearing expiry

---
+++ src/Components/NearlyExpiryItems.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router';
+
+function NearlyExpiryItems() {
+    const [nearlyExpiry,setnearlyExpiry] = useState([])
+
+   useEffect(() => {
+    fetch("http://localhost:3000/nearly-expiry") 
+      .then(res => res.json())
+      .then(data => {
+        
+        const nearlyExpiryItems = data.slice(0,6); 
+        setnearlyExpiry(nearlyExpiryItems);
+      })
+     
+  }, []);
+
+  return (
+    <div className='py-20 sm:px-30 px-4  bg-base-200 text-base w-full '>
+        <h2 className='text-center text-3xl text-accent font-semibold'>Nearly <span className='text-secondary'>Expiry</span> Items</h2>
+        {
+            nearlyExpiry.length<=0 && 
+            <div className='mt-4 bg-white p-5 shadow-md'>
+                <p className='text-2xl font-medium text-center text-primary'>NO Nearly Expiry Food Yet</p>
+            </div>
+        }
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-5">
+                {
+                nearlyExpiry.map((food)=>(
+                    <div key={food._id} className="card bg-white shadow-xl transition-transform hover:scale-105 p-4 rounded-xl">
+                        <img
+                  src={food.photo}
+                  alt=""
+                  className="w-full h-[200px] object-cover rounded-xl"
+                />
+             
+              <div className="card-body  ">
+                <h3 className="card-title">{food.title}</h3>
+                <p className="text-sm text-gray-700  mb-1">{food.description}</p>
+                <p className="text-sm text-gray-500 ">Category: <span className="font-medium">{food.category}</span></p>
+                <p className="text-sm text-gray-500 ">Quantity: <span className="font-medium">{food.quantity}</span></p>
+                <p className="text-sm text-gray-500 ">Expiry Date: <span className="font-medium">{food.expirydate}</span></p>
+               <Link to={`/fridge/${food._id}`}> <button className="btn btn-sm btn-primary  mt-4 cursor-pointer hover:bg-accent duration-200 border-none py-5">
+                  See Details
+                </button></Link>
+              </div>
+                    </div>
+                ))
+              }
+              </div>
+    </div>
+  )
+}
+
+export default NearlyExpiryItems
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../Components/Banner'
+import NearlyExpiryItems from '../Components/NearlyExpiryItems'
 
 function Home() {
   return (
     <div>
       <Banner/>
+      <NearlyExpiryItems/>
     </div>
   )
 }
